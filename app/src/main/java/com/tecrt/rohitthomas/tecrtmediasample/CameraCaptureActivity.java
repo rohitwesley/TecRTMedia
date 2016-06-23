0ca9ed95e7d4880e42082fb512d29a25e7152b95
rohitwes <rohitwesley@gmail.com>
2016-06-23 22:14:42 +0530

    Raw file with camera and icons

---
+++ app/src/main/java/com/tecrt/rohitthomas/tecrtmediasample/CameraCaptureActivity.java
@@ -0,0 +1,277 @@
+package com.tecrt.rohitthomas.tecrtmediasample;
+
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.opengl.GLSurfaceView;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+
+public class CameraCaptureActivity extends AppCompatActivity implements SurfaceTexture.OnFrameAvailableListener {
+    private static final String TAG = "CameraCaptureActivity";
+    private static final boolean VERBOSE = false;
+
+    // Camera filters; must match up with cameraFilterNames in strings.xml
+    static final int FILTER_NONE = 0;
+
+    private GLSurfaceView mGLView;
+    private CameraSurfaceRenderer mRenderer;
+    private Camera mCamera;
+    private CameraHandler mCameraHandler;
+    private boolean mRecordingEnabled;      // controls button state
+
+    private int mCameraPreviewWidth, mCameraPreviewHeight;
+
+    // this is static so it survives activity restarts
+    private static TextureMovieEncoder sVideoEncoder = new TextureMovieEncoder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_capture);
+
+        File outputFile = new File(Environment.getExternalStorageDirectory(), "stream.mp4");
+
+
+        // Define a handler that receives camera-control messages from other threads.  All calls
+        // to Camera must be made on the same thread.  Note we create this before the renderer
+        // thread, so we know the fully-constructed object will be visible.
+        mCameraHandler = new CameraHandler(this);
+
+        mRecordingEnabled = sVideoEncoder.isRecording();
+
+        // Configure the GLSurfaceView.  This will start the Renderer thread, with an
+        // appropriate EGL context.
+        mGLView = (GLSurfaceView) findViewById(R.id.cameraPreview_surfaceView);
+        mGLView.setEGLContextClientVersion(2);     // select GLES 2.0
+        mRenderer = new CameraSurfaceRenderer(mCameraHandler, sVideoEncoder, outputFile);
+        mGLView.setRenderer(mRenderer);
+        mGLView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+
+
+        Log.d(TAG, "onCreate complete: " + this);
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume -- acquiring camera");
+        super.onResume();
+
+        if (mCamera == null) {
+            openCamera(1920, 1080);
+        }
+
+
+
+
+        mGLView.queueEvent(new Runnable() {
+            @Override
+            public void run() {
+                mRenderer.setCameraPreviewSize(mCameraPreviewWidth, mCameraPreviewHeight);
+            }
+        });
+
+        mGLView.onResume();
+
+
+
+
+        //
+
+        Log.d(TAG, "onResume complete: " + this);
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause -- releasing camera");
+        super.onPause();
+
+        Log.d(TAG, "onPause complete");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop started");
+        super.onStop();
+
+        releaseCamera();
+        mGLView.queueEvent(new Runnable() {
+            @Override public void run() {
+                // Tell the renderer that it's about to be paused so it can clean up.
+                mRenderer.notifyPausing();
+            }
+        });
+        mGLView.onPause();
+        Log.d(TAG, "onStop complete");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+        mCameraHandler.invalidateHandler();     // paranoia
+    }
+
+
+    private void openCamera(int desiredWidth, int desiredHeight) {
+        if (mCamera != null) {
+            throw new RuntimeException("camera already initialized");
+        }
+
+        mCamera = Camera.open();    // opens first back-facing camera
+
+        if (mCamera == null) {
+            throw new RuntimeException("Unable to open camera");
+        }
+
+        Camera.Parameters parms = mCamera.getParameters();
+
+        CameraUtils.choosePreviewSize(parms, desiredWidth, desiredHeight);
+
+        // Give the camera a hint that we're recording video.  This can have a big
+        // impact on frame rate.
+        parms.setRecordingHint(true);
+
+        // leave the frame rate set to default
+        mCamera.setParameters(parms);
+
+        Camera.Size mCameraPreviewSize = parms.getPreviewSize();
+
+
+        mCameraPreviewWidth = mCameraPreviewSize.width;
+        mCameraPreviewHeight = mCameraPreviewSize.height;
+    }
+
+    /**
+     * Stops camera preview, and releases the camera to the system.
+     */
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+            Log.d(TAG, "releaseCamera -- done");
+        }
+    }
+
+    /**
+     * onClick handler for "record" button.
+     */
+    public void clickToggleRecording(@SuppressWarnings("unused") View unused) {
+        mRecordingEnabled = !mRecordingEnabled;
+        mGLView.queueEvent(new Runnable() {
+            @Override public void run() {
+                // notify the renderer that we want to change the encoder's state
+                mRenderer.changeRecordingState(mRecordingEnabled);
+            }
+        });
+        updateControls();
+    }
+
+//    /**
+//     * onClick handler for "rebind" checkbox.
+//     */
+//    public void clickRebindCheckbox(View unused) {
+//        CheckBox cb = (CheckBox) findViewById(R.id.rebindHack_checkbox);
+//        TextureRender.sWorkAroundContextProblem = cb.isChecked();
+//    }
+
+    /**
+     * Updates the on-screen controls to reflect the current state of the app.
+     */
+    private void updateControls() {
+        Button toggleRelease = (Button) findViewById(R.id.toggleRecording_button);
+        int id = mRecordingEnabled ?
+                R.string.toggleRecordingOff : R.string.toggleRecordingOn;
+        toggleRelease.setText(id);
+
+        //CheckBox cb = (CheckBox) findViewById(R.id.rebindHack_checkbox);
+        //cb.setChecked(TextureRender.sWorkAroundContextProblem);
+    }
+
+    /**
+     * Connects the SurfaceTexture to the Camera preview output, and starts the preview.
+     */
+    private void handleSetSurfaceTexture(SurfaceTexture st) {
+        st.setOnFrameAvailableListener(this);
+        try {
+            mCamera.setPreviewTexture(st);
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+        mCamera.startPreview();
+    }
+
+    @Override
+    public void onFrameAvailable(SurfaceTexture st) {
+        // The SurfaceTexture uses this to signal the availability of a new frame.  The
+        // thread that "owns" the external texture associated with the SurfaceTexture (which,
+        // by virtue of the context being shared, *should* be either one) needs to call
+        // updateTexImage() to latch the buffer.
+        //
+        // Once the buffer is latched, the GLSurfaceView thread can signal the encoder thread.
+        // This feels backward -- we want recording to be prioritized over rendering -- but
+        // since recording is only enabled some of the time it's easier to do it this way.
+        //
+        // Since GLSurfaceView doesn't establish a Looper, this will *probably* execute on
+        // the main UI thread.  Fortunately, requestRender() can be called from any thread,
+        // so it doesn't really matter.
+        if (VERBOSE) Log.d(TAG, "ST onFrameAvailable");
+        mGLView.requestRender();
+    }
+
+    /**
+     * Handles camera operation requests from other threads.  Necessary because the Camera
+     * must only be accessed from one thread.
+     * <p>
+     * The object is created on the UI thread, and all handlers run there.  Messages are
+     * sent from other threads, using sendMessage().
+     */
+    static class CameraHandler extends Handler {
+        public static final int MSG_SET_SURFACE_TEXTURE = 0;
+
+        // Weak reference to the Activity; only access this from the UI thread.
+        private WeakReference<CameraCaptureActivity> mWeakActivity;
+
+        public CameraHandler(CameraCaptureActivity activity) {
+            mWeakActivity = new WeakReference<CameraCaptureActivity>(activity);
+        }
+
+        /**
+         * Drop the reference to the activity.  Useful as a paranoid measure to ensure that
+         * attempts to access a stale Activity through a handler are caught.
+         */
+        public void invalidateHandler() {
+            mWeakActivity.clear();
+        }
+
+        @Override  // runs on UI thread
+        public void handleMessage(Message inputMessage) {
+            int what = inputMessage.what;
+            Log.d(TAG, "CameraHandler [" + this + "]: what=" + what);
+
+            CameraCaptureActivity activity = mWeakActivity.get();
+            if (activity == null) {
+                Log.w(TAG, "CameraHandler.handleMessage: activity is null");
+                return;
+            }
+
+            switch (what) {
+                case MSG_SET_SURFACE_TEXTURE:
+                    activity.handleSetSurfaceTexture((SurfaceTexture) inputMessage.obj);
+                    break;
+                default:
+                    throw new RuntimeException("unknown msg " + what);
+            }
+        }
+    }
+}
