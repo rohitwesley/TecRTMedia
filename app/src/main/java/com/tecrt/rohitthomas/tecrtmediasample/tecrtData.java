b10d829c1f9e9a983dab60483116212751bd1d17
rohitwes <rohitwesley@gmail.com>
2016-06-23 22:14:42 +0530

    Raw file with camera and icons

---
+++ app/src/main/java/com/tecrt/rohitthomas/tecrtmediasample/tecrtData.java
@@ -0,0 +1,31 @@
+package com.tecrt.rohitthomas.tecrtmediasample;
+
+import android.app.ProgressDialog;
+
+/**
+ * Created by rohitthomas on 06/06/16.
+ */
+public class tecrtData {
+    // Preview view aspect ratio.
+    public float mAspectRatioPreview[] = new float[2];
+    // Video Rez.
+    public int videoWidth;
+    public int videoHeight;
+    // Filter values.
+    public float mBrightness = 1, mContrast = 1, mSaturation = 1, mCornerRadius = 1;
+    // Predefined filter.
+    public int mFilter = 0;
+
+
+    // Taken picture data (jpeg).
+    public byte[] mImageData;
+    // Progress dialog while saving picture.
+    public ProgressDialog mImageProgress;
+    // Picture capture time.
+    public long mImageTime;
+    // Device orientation degree.
+    public int mOrientationDevice;
+    // Camera orientation matrix.
+    public final float mOrientationM[] = new float[16];
+
+}
